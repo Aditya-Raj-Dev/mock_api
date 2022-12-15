15248e17b9ed9a7c494eb277c9f88cc901bfecf5
Aditya Raj <aadiyadav56837492@gmail.com>
2022-12-15 13:54:10 +0530

    j

--- database/db.js
+++ database/db.js
@@ -1,4 +1,4 @@
 const  mongoose = require("mongoose")
-const connection =mongoose.connect("mongodb+srv://adi:123@cluster0.6osan8b.mongodb.net/test")
+const connection =mongoose.connect("mongodb+srv://adi:123@cluster0.yzrar1c.mongodb.net/test")
 
 module.exports=connection
--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const connection = require("./database/db")
 const UserModel=require("./Model/model")
 const EmiModel =require("./Model/Emi.model")
 const cors = require("cors")
+const ListModel = require("./Model/list.model")
+const ListRouter = require("./Route/list.route")
 
 const app=express()
 app.use(express.json())
@@ -11,42 +13,39 @@ app.use(cors())
 app.get("/",async(req,res)=>{
     res.send("welcome")
 })
-app.get("/getprofile",async(req,res)=>{
-    const {email}=req.body;
-    console.log(email)
-    const data=await EmiModel.find({email:email})
-    res.send({"msg":data})
-})
 
-app.post("/emicalculate",async(req,res)=>{
-    const {email,loan,annual,month}=req.body;
-    let rate=annual/12/100
-  let EMI= Math.trunc(loan*rate*(month+(rate*month))/((month+rate)-1))
-    const data=await EmiModel.create({
-        email,loan,annual,month,EMI
-    })
-    res.send({"msg":EMI})
-})
+app.use("/list",ListRouter)
 
-app.post("/register",(req,res)=>{
-  const {name,email,password}=req.body;
-  const data=UserModel.create({
-    name,email,password
-  })
-  res.send({"msg":"Signup Successfull"})
-})
 
-app.post("/login",async (req,res)=>{
-    const {email,password}=req.body;
-    const user=await UserModel.findOne({email})
-    console.log(user)
-    if(user.email===email && user.password===password){
-        res.send({"msg":"Login Successfull"})
-    }
-    else{
-        res.send({"msg":"Login Failed"})
-    }
-  })
+// app.post("/emicalculate",async(req,res)=>{
+//     const {email,loan,annual,month}=req.body;
+//     let rate=annual/12/100
+//   let EMI= Math.trunc(loan*rate*(month+(rate*month))/((month+rate)-1))
+//     const data=await EmiModel.create({
+//         email,loan,annual,month,EMI
+//     })
+//     res.send({"msg":EMI})
+// })
+
+// app.post("/register",(req,res)=>{
+//   const {name,email,password}=req.body;
+//   const data=UserModel.create({
+//     name,email,password
+//   })
+//   res.send({"msg":"Signup Successfull"})
+// })
+
+// app.post("/login",async (req,res)=>{
+//     const {email,password}=req.body;
+//     const user=await UserModel.findOne({email,password})
+//     console.log(user)
+//     if(user.email===email && user.password===password){
+//         res.send({"msg":"Login Successfull"})
+//     }
+//     else{
+//         res.send({"msg":"Login Failed"})
+//     }
+//   })
 
 app.listen(8080,async()=>{
     try{
